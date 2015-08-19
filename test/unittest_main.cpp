8efecd4500a989bb8dbfd12a0418d57d3b0cbe29
stubbfel <dev@stubbfel>
2015-08-19 22:07:03 +0200

    rm old giles

--- test/unittest_main.cpp
+++
@@ -1,9 +0,0 @@
-#include "UnitTest++/UnitTest++.h"
-#include "../src/map/natmap.h"
-#include <tins/tins.h>
-#include <vector>
-
-int main(int, const char *[])
-{
-    return UnitTest::RunAllTests();
-}
