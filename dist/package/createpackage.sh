1a7229e8b104717903f48de906a4f4723608d7fa
stubbfel <dev@stubbfel>
2015-11-23 23:37:14 +0100

    fix package script

--- dist/package/createpackage.sh
+++ dist/package/createpackage.sh
@@ -6,12 +6,15 @@ cp -R ../../lib/src/ temp/121Nat/lib/
 cp ../../lib/*.sh temp/121Nat/lib/
 cp ../../build.sh temp/121Nat/
 cp ../../CMakeLists.txt temp/121Nat/
-cp PKGBUILD temp/
+cp PKGBUILD.example temp/PKGBUILD
 
 cd temp
 tar -cvzf 121Nat-1.0.tar.gz 121Nat/
 md5=($(md5sum 121Nat-1.0.tar.gz))
 echo "md5sums=('$md5')" >> PKGBUILD
 makepkg
-cp temp/*.pkg.tar.xz .
-rm -Rf temp/121Nat/
+cp *.pkg.tar.xz ../
+cp *.tar.gz ../
+cp -f PKGBUILD ../PKGBUILD
+cd ..
+rm -Rf temp
